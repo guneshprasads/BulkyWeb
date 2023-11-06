1388dce26ac3b83401bcbb344c53cd930f166a2b
guneshprasads <guneshprasads2506@gmail.com>
2023-11-06 09:04:36 +0530

    Initial commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace BulkyWeb.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View("Privacy");
         }
 
         public IActionResult Privacy()
